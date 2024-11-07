8bd64c0e4e54046dea77f0ecf810085ae1ea9b76
gerasantillan17 <gerapumas122@gmail.com>
2024-11-06 23:59:52 -0600

    Add files via upload

---
+++ JavaApplication42.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication42;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int val=1;
+        while (val<=10){
+            System.out.println(val + "-");
+            val++;
+        }
+        System.out.println();
+
+        val=1;
+        do{
+            System.out.println(val + "-");
+            val++;
+        }while(val<=10);
+    }
+    
+    
+}
